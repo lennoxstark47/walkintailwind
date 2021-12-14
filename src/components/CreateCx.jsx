35b04174c9deb2ef7d1b30676c9b2ad17eb62fd6
stark <twisamhaldar@gmail.com>
2021-12-14 23:45:45 +0530

    useless commit

--- src/components/CreateCx.jsx
+++ src/components/CreateCx.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class CreateCx extends Component {
 	render() {
 		return (
-			<div>
+			<div className='flex justify-center h-full'>
+				<div
+					className='flex md:w-3/6 lg:w-1/5 h-96 sm:w-2/3
+				 mt-10 border-2 bg-slate-600 justify-center content-center'></div>
 			</div>
 		);
 	}
