84f0364578b239cae5754e84fdd2d9c3dbd60254
Stark 47 <twisamhaldar@gmail.com>
2022-01-11 00:42:07 +0530

    almost completed

--- src/components/EditCx.jsx
+++ src/components/EditCx.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Switch from '@mui/material/Switch';
 
 export default class EditCx extends Component {
 	constructor(props) {
@@ -32,13 +33,83 @@ export default class EditCx extends Component {
 			});
 	}
 
+	onRemarksChange = (event) => {
+		this.setState({
+			remarks: event.target.value,
+		});
+	};
+
+	onToggle = (event) => {
+		if (this.state.isConverted) {
+			this.setState({ isConverted: false });
+		} else {
+			this.setState({ isConverted: true });
+		}
+	};
+
+	onNameChange = (event) => {
+		this.setState({
+			name: event.target.value,
+		});
+	};
+
+	onAddressChange = (event) => {
+		this.setState({
+			address: event.target.value,
+		});
+	};
+
+	onPhoneChange = (event) => {
+		this.setState({
+			phone: event.target.value,
+		});
+	};
+
+	onPinChange = (event) => {
+		this.setState({
+			pin: event.target.value,
+		});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		const updatedCx = {
+			remarks: this.state.remarks,
+			isConverted: this.state.isConverted,
+			name: this.state.name,
+			address: this.state.address,
+			phone: this.state.phone,
+			pin: this.state.pin,
+		};
+
+		axios
+			.put(
+				'https://secure-dusk-73088.herokuapp.com/api/customer/' +
+					this.props.match.params.id,
+				updatedCx
+			)
+			.then((res) => {
+				if (!res) {
+					console.log('error');
+				} else {
+					window.location = '/success';
+				}
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	render() {
 		return (
 			<div className='min-h-screen  items-center'>
 				<div className='container mx-auto w-5/6  rounded-lg max-w-md shadow-lg hover:shadow-lg transition duration-300'>
 					<div className='py-12  p-10  rounded-lg mt-5 h-full  bg-violet-200'>
+						<p className='mr-4 text-gray-700 font-bold text-lg inline-block mb-2 '>
+							{this.state.name}
+						</p>
 						<div className='mb-6'>
-							<h4>{this.state.name}</h4>
 							<label className='mr-4 text-gray-700 font-bold inline-block mb-2 '>
 								Name
 							</label>
@@ -75,6 +146,31 @@ export default class EditCx extends Component {
 								className='mr-4 bg-white w-full text-gray-500 py-2 px-4 lg:w-96 border-transparent focus:ring-2 focus:ring-indigo-400 rounded'
 								onChange={this.onPinChange}
 							/>
+							<label className='mr-4 mt-3 text-gray-700 font-bold inline-block mb-2 '>
+								Remarks
+							</label>
+							<textarea
+								value={this.state.remarks}
+								onChange={this.onRemarksChange}
+								typeof='text'
+								className='mr-4 bg-white w-full text-gray-500 py-2 px-4 lg:w-96 border-transparent focus:ring-2 focus:ring-indigo-400 rounded'
+							/>
+							<div style={{ marginLeft: '5px' }}>
+								{/* <button
+										type='button'
+										className='btn btn-primary'
+										onClick={this.onToggle}>
+										Convert
+									</button> */}
+								<span>Convert</span>
+								<Switch
+									checked={this.state.isConverted}
+									onChange={this.onToggle}
+									inputProps={{
+										'aria-label': 'controlled',
+									}}
+								/>
+							</div>
 							<button
 								onClick={this.handleSubmit}
 								className='lg:w-96 w-full mt-6 text-indigo-50 font-bold bg-indigo-600 py-3 rounded-md hover:bg-indigo-500 transition duration-300'>
